077336b94ded6ec2ffb722393bb746f0565d3353
John Grib <johngrib@woowahan.com>
2017-01-08 11:37:38 +0900

    jooq 튜토리얼 코드 추가

--- src/main/java/com/reservation/ReservationApplication.java
+++ src/main/java/com/reservation/ReservationApplication.java
@@ -1,12 +1,19 @@
 package com.reservation;
 
+import com.reservation.jooq.tables.Reservation;
 import com.reservation.repository.ReservationRepository;
-import com.reservation.entity.Reservation;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -18,8 +25,8 @@ public class ReservationApplication {
 
             rr.deleteAll();
 
-            Arrays.asList("Dr. Rod,Dr. Syer,Juergen,Spencer,Phillip,ALL THE COMMUNITY,Josh".split(","))
-                    .forEach(x -> rr.save(new Reservation(x)));
+            //Arrays.asList("Dr. Rod,Dr. Syer,Juergen,Spencer,Phillip,ALL THE COMMUNITY,Josh".split(","))
+                    //.forEach(x -> rr.save(new Reservation(x)));
 
             rr.findAll().forEach(System.out::println);
         };
@@ -27,6 +34,30 @@ public class ReservationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReservationApplication.class, args);
+
+        String userName = "이름";
+        String password = "패스워드";
+        String url = "jdbc:mysql://접속주소:3306/study";
+
+        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+            System.out.println("connected!");
+
+            DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+            Result<Record> result = create.select().from(Reservation.RESERVATION).fetch();
+
+            // 자동생성된 쿼리를 확인해 보자.
+            String sql = create.select().from(Reservation.RESERVATION).getSQL();
+            System.out.println(sql);
+
+            // 쿼리 실행 결과를 확인해 보자.
+            System.out.println("??????????");
+            result.stream().forEach(System.out::println);
+            System.out.println("!!!!!!!!!!");
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
