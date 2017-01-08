a94f151fb1920ccb0aef67429e6f71ebe3eb833f
John Grib <johngrib@woowahan.com>
2017-01-08 15:14:17 +0900

    jooq 컨트롤러에 맞춰 테스트 코드 수정

--- src/test/java/com/example/ReservationTests.java
+++ src/test/java/com/example/ReservationTests.java
@@ -1,13 +1,50 @@
 package com.example;
 
+import com.reservation.ReservationApplication;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertEquals;
 
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ReservationApplication.class)
+@WebAppConfiguration
 public class ReservationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext webContext;    // WebApplicationContext 주입
+    private MockMvc mockMvc;
 
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+    }
+
+    @Test
+    public void reservationTest() throws Exception {
+
+        mockMvc.perform(
+            MockMvcRequestBuilders
+                .get("/reservation/find-by-name/johngrib")    // 이렇게 하면 지정한 주소로 request를 보낼 수 있다
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(MockMvcResultMatchers.status().isOk())   // 응답 확인
+        .andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.[0].name").value("johngrib"))
+        ;
+    }
 }
